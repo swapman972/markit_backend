12a64c00910c2d8a3e5dc9774b4e5386d5f11a47
swapman972 <jordan.melidor@hotmail.fr>
2020-07-07 11:52:43 -0400

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
          end
     end
 
-
     def edit
         @user = User.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  resources :trains
-  resources :bikes
-  resources :ratings
-  resources :events
+  resources :trains, only: [:index]
+  resources :bikes, only: [:index]
+  resources :ratings, only: [:index, :create]
+  resources :events, only: [:index, :show, :create]
   resources :users
-  # get "/events/restaurant", to: "events#restaurant"
-  # get "/events/pool", to: "events#pool"
-  # get "/events/coffee", to: "events#coffee"
-  # get "/events/music", to: "events#music"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
